948ea811fd0b4b41a1f9c70e46483447857f5762
BlackBaroness <shoyurash2@gmail.com>
2021-08-19 12:25:56 +0300

    initial commit

---
+++ src/main/java/ru/baronessdev/lib/cloud/BaronessCloud.java
@@ -0,0 +1,17 @@
+package ru.baronessdev.lib.cloud;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BaronessCloud extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
